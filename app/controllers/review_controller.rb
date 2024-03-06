86030986003aa84fafdabd9c096564dcd25c16fb
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-05 21:44:34 -0300

    added create action

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,3 +1,11 @@
 class ReviewController < ApplicationController
-
+  def create
+    @review = Review.new review_params
+    @review.restaurant = @restaurant = params[:restaurant_id]
+    if @review.save
+      redirect_to @restaurant, notice: "Restaurant was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
