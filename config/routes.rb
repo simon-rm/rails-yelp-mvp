bd395444903fc6487a11651fedce1f371a1db3c0
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-05 21:34:56 -0300

    added review create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants, only: %i[index show new create] do # user spec edit?
+    resources :reviews, only: :create
     member do
       get :reviews
     end
